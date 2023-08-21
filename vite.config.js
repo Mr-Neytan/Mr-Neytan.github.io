0e114d7c1e80cc2585144af739797bf7b674061c
nathaniel-mercado <nmercado0517@gmail.com>
2023-08-22 03:18:14 +0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Mr-Neytan.github.io",
+  base: "Mr-Neytan.github.io",
   plugins: [react()],
 })
