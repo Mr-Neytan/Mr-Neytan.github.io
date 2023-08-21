a16ef11dff91f41c9952a359765e1d48292bc233
nathaniel-mercado <nmercado0517@gmail.com>
2023-08-22 02:37:47 +0800

    initial files and designs

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Welcome from './Components/Welcome/Welcome'
+
+function App() {
+
+  return (
+    <>
+      <Welcome/>
+      <hr></hr>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Welcome/Welcome.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./welcome.css"
+import "../../../public/pic_2.jpg"
+import "../../../public/fb_logo.png"
+import "../../../public/linkedin_logo.png"
+import "../../../public/gmail_logo.png"
+import "../../../public/github_logo.png"
+const Welcome = () => {
+    const pic = "../../../public/pic_2.jpg"
+  return (
+    <>
+    <div className="banner">
+
+    <div className='profile_pic'>
+        <img src={pic}></img>
+    </div>
+    <div className="welcome_text">
+    <h1>Hello, I am <span className='emphasis'>Nathan</span>!</h1>
+    <h2><span>— A </span> <span className="emphasis">Web Developer</span>.</h2>
+    <p>I have a passion for designing and developing functional, efficient and attractive web apps. I am constantly searching for ways to integrate other technologies into web development such as AI. I hope to be part of the next big thing in technology.</p>
+    <div className='contact_info'>
+        <h3>Let's Connect:</h3>
+        <ul className='contact_list'>
+            <a href="https://www.facebook.com/SuperN0517" target='blank'><li className='contact_link'><img src='../../../public/fb_logo.png'></img></li></a>
+            <a href="https://www.linkedin.com/in/nathaniel-mercado-35a013242/" target='blank'><li className='contact_link'><img src="../../../public/linkedin_logo.png"></img></li></a>
+            <a href="mailto:nathan.mercado0517@gmail.com" target='blank'><li className='contact_link'><img src="../../../public/gmail_logo.png"></img></li></a>
+            <a href="https://github.com/Mr-Neytan" target='blank'><li className='contact_link'><img src="../../../public/github_logo.png"></img></li></a>
+        </ul>
+    </div>
+    
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Welcome
